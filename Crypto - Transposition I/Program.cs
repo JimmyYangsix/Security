1c86cd1334bec87cb6d5bd65e7ea3142445a8610
JimmyYangsix <1075887609@qq.comm>
2018-03-31 19:07:07 +0800

    v0.0.1

---
+++ Crypto - Transposition I/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto___Transposition_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            Console.WriteLine("请输入字符串");
+            string input = Console.ReadLine();
+            char[] slove = input.ToCharArray();
+            char transposition = slove[0];
+            for (int i = 0; i < slove.Length; i++)
+            {
+                if(i%2==1)
+                {
+                    transposition = slove[i - 1];
+                    slove[i - 1] = slove[i];
+                    slove[i] = transposition;
+                }
+            }
+            
+            for(int j = 0;j<slove.Length;j++)
+            {
+                output += slove[j];
+            }
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+}
